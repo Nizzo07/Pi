39ff9ff64a64ed37ce053f619f12aa8499649371
Nizzo07 <afonso7557@gmail.com>
2023-05-12 19:04:24 +0100

    12/05/23

---
+++ structs/passingstructsToFunc.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+typedef struct student
+{
+    char name[50];
+    int age;
+}student;
+
+void display(student s);
+
+int main()
+{
+    student s1;
+
+    printf("Enter name: ");
+    //read string input from the user untill \n is entered
+    //\n is discarded
+
+    scanf("%[^\n]%c", s1.name);
+
+    printf("Enter age: ");
+    scanf("%d", &s1.age);
+
+    display(s1);
+    return 0;
+}
+
+void display(student s)
+{
+    printf("\nDisplaying information...\n");
+    printf("...\n...\n");
+    printf("\nName: %s\n", s.name);
+    printf("Age: %d", s.age);
+}
