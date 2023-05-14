de0944ab1aeecbb36db8dc99e9af044c05592de1
Nizzo07 <afonso7557@gmail.com>
2023-05-14 22:44:17 +0100

    okii

--- Trees/treeTraversal.c
+++ Trees/treeTraversal.c
@@ -1,52 +1,52 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//Tree Traversal 
+// Tree Traversal
 
 struct node
 {
     int item;
-    struct node* left;
-    struct node* right;
+    struct node *left;
+    struct node *right;
 };
 
-//Inorder Traversal
-void InorderTraversal(struct node* root)
+// Inorder Traversal
+void InorderTraversal(struct node *root)
 {
-    if(root == NULL)
-    return;
+    if (root == NULL)
+        return;
 
     InorderTraversal(root->left);
     printf("%d ->", root->item);
     InorderTraversal(root->right);
 }
 
-//PreorderTraversal Traversal
-void preorderTraversal(struct node* root)
+// PreorderTraversal Traversal
+void preorderTraversal(struct node *root)
 {
-    if(root == NULL)
-    return;
+    if (root == NULL)
+        return;
 
     printf("%d ->", root->item);
     preorderTraversal(root->left);
-    preorderTraversal(root->right);    
+    preorderTraversal(root->right);
 }
 
-//PostorderTraversal Traversal
-void postorderTraversal(struct node* root)
+// PostorderTraversal Traversal
+void postorderTraversal(struct node *root)
 {
-    if(root == NULL)
-    return;
+    if (root == NULL)
+        return;
 
     postorderTraversal(root->left);
     postorderTraversal(root->right);
     printf("%d ->", root->item);
 }
 
-//Create a new node
-struct node* createNode(value)
+// Create a new node
+struct node *createNode(value)
 {
-    struct node* newNode = malloc(sizeof(struct node));
+    struct node *newNode = malloc(sizeof(struct node));
     newNode->item = value;
     newNode->left = NULL;
     newNode->right = NULL;
@@ -55,14 +55,14 @@ struct node* createNode(value)
 }
 
 // Insert on the left of the node
-struct node* insertLeft(struct node* root, int value)
+struct node *insertLeft(struct node *root, int value)
 {
     root->left = createNode(value);
     return root->left;
 }
 
-//Insert on the right of the node
-struct node* insertRight(struct node* root, int value)
+// Insert on the right of the node
+struct node *insertRight(struct node *root, int value)
 {
     root->right = createNode(value);
     return root->right;
@@ -70,7 +70,7 @@ struct node* insertRight(struct node* root, int value)
 
 int main()
 {
-    struct node* root = createNode(1);
+    struct node *root = createNode(1);
     insertLeft(root, 12);
     insertRight(root, 6);
 
@@ -85,5 +85,4 @@ int main()
 
     printf("\nPostorder Traversal \n");
     postorderTraversal(root);
-
 }
