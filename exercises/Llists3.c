cf52766b899c441645ba5871bd1f4c9f44bcece2
Nizzo07 <afonso7557@gmail.com>
2023-05-16 22:17:33 +0100

    Exerc3

---
+++ exercises/Llists3.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+    int x;
+    struct Node* next;
+} Node;
+
+void insert_end(Node** root, int value)
+{
+    Node* new_node = malloc(sizeof(Node));
+    if(new_node == NULL)
+    {
+        exit (1);
+    } else {
+    new_node->next = NULL;
+    new_node->x = value;
+    }
+    
+    Node* curr = *root;
+    
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = new_node;
+
+} 
+
+int main()
+{
+    Node* root = malloc(sizeof(Node));
+    if(root == NULL)
+    {
+        exit (2);
+    }
+
+    root->x = 15;
+    root->next = NULL;
+
+    insert_end(&root,-2);
+    insert_end(&root, 11);
+
+
+  for(Node* curr = root; curr != NULL; curr = curr->next)
+  {
+    printf("%d\n", curr->x);
+  }
+
+    return 0;
+}
